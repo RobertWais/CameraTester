1947eb3df39f81f23b5d96758f5e1874d85bb222
RobertWais <wais.robert@uwlax.edu>
2018-03-05 15:16:25 -0600

    Initial commit

--- CameraTester/ViewController.swift
+++ CameraTester/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pressedBtn(_ sender: Any) {
+    }
+    
 }
 
+extension ViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    @IBAction func btnPressed(_ sender: UIButton){
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        if sender.tag == 0 {
+            imagePicker.sourceType = .camera
+        }else if sender.tag == 1{
+            print("2")
+            imagePicker.sourceType = .photoLibrary
+        }
+        self.present(imagePicker,animated:true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        let shownImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = shownImage
+    }
+    
+}
